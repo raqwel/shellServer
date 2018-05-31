9f4c6d4f9fa438555ff719c73a4324b62b48f12e
raqwel <yuto.imamu@gmail.com>
2018-06-01 00:02:50 +0900

    awkの中で関数を実行したい

--- shellServer.sh
+++ shellServer.sh
@@ -1,17 +1,23 @@
+#!/usr/bin/env bash
 /usr/bin/mkfifo backpipe
 
-while [[ true ]]; do
-  # ファイル名取得
-  fileName=$(nc -l 3000 | awk '/HTTP/{print substr($2, 2)}')
-
-  # HTTPレスポンスｎ必要なものを用意
+createHttpResponse() {
+# HTTPレスポンスｎ必要なものを用意
   httpStatus="HTTP/1.1 200 OK"
   crlf="\r\n"
   messageBody=$(cat "${fileName}")
-
+  # ex) Date: Tue, 15 Nov 1994 08:12:31 GMT
+#  LANG=en_US.UTF-8
+  responseDate=$(LANG=en_US.UTF-8 date "+%a, %d %b %Y %T GMT")
+  httpDate="Date: ${responseDate}"
   # HTTPレスポンス組み立て
-  httpResponse=${httpStatus}${crlf}${crlf}${messageBody}
+
+  httpResponse=${httpStatus}${crlf}${httpDate}${crlf}${crlf}${messageBody}
 
   # レスポンス
-  echo ${httpResponse} | nc -l 3000
+  echo ${httpResponse}
+}
+while [[ true ]]; do
+#  awkの中で関数を実行したい
+  nc -l 3000 0<backpipe | awk '/HTTP/{system("bash -c 'createHttpResponse' substr($2, 2)")}' 1>backpipe
 done
