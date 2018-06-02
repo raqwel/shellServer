a4e844c6b4505109fd18998f84fdcbaeffe0e236
raqwel <yuto.imamu@gmail.com>
2018-06-02 20:13:01 +0900

    dateがGMTじゃなかった

--- shellServer.sh
+++ shellServer.sh
@@ -10,7 +10,7 @@ createHttpResponse() {
     httpStatus="HTTP/1.1 200 OK"
     crlf="\r\n"
     messageBody=$(cat ${filename})
-    responseDate=$(LANG=en_US.UTF-8 date "+%a, %d %b %Y %T GMT")
+    responseDate=$(LANG=en_US.UTF-8 date -uR "+%a, %d %b %Y %T GMT")
     httpDate="Date: ${responseDate}"
     # HTTPレスポンス組み立て
     httpResponse=${httpStatus}${crlf}${httpDate}${crlf}${crlf}${messageBody}
@@ -19,15 +19,11 @@ createHttpResponse() {
     echo ${httpResponse}
 }
 
-test() {
-    IFS=" "
-    set -- ${1}
-    createHttpResponse ${2#/}
-}
 #  awkの中で関数を実行したい →　どうしてもできない
 #  nc -l 3000 0<backpipe | awk '/HTTP/{system("bash -c " createHttpResponse substr($2, 2))}' 1>backpipe
 set -x
 while [[ true ]]; do
+    # readがないと、HTTPリクエストがないのに処理が行き過ぎてしまうので、headの結果を待つ→リクエストをしっかり受け取ることを行う
     nc -l 3000 -w 1 0<backpipe | head -n1 | (read request; createHttpResponse "${request}") 1>backpipe
 done
 
