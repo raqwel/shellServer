9777fdcc4062ece54f1c161caa4d1035d8c6afa4
raqwel <yuto.imamu@gmail.com>
2018-06-02 15:55:01 +0900

    とりまできた

--- shellServer.sh
+++ shellServer.sh
@@ -2,22 +2,28 @@
 /usr/bin/mkfifo backpipe
 
 createHttpResponse() {
-# HTTPレスポンスｎ必要なものを用意
-  httpStatus="HTTP/1.1 200 OK"
-  crlf="\r\n"
-  messageBody=$(cat "${fileName}")
-  # ex) Date: Tue, 15 Nov 1994 08:12:31 GMT
-#  LANG=en_US.UTF-8
-  responseDate=$(LANG=en_US.UTF-8 date "+%a, %d %b %Y %T GMT")
-  httpDate="Date: ${responseDate}"
-  # HTTPレスポンス組み立て
+    # HTTPレスポンスに必要なものを用意
+    httpStatus="HTTP/1.1 200 OK"
+    crlf="\r\n"
+    messageBody=$(cat ${1})
+    responseDate=$(LANG=en_US.UTF-8 date "+%a, %d %b %Y %T GMT")
+    httpDate="Date: ${responseDate}"
+    # HTTPレスポンス組み立て
+    httpResponse=${httpStatus}${crlf}${httpDate}${crlf}${crlf}${messageBody}
 
-  httpResponse=${httpStatus}${crlf}${httpDate}${crlf}${crlf}${messageBody}
-
-  # レスポンス
-  echo ${httpResponse}
+    # レスポンス
+    echo ${httpResponse}
 }
+
+test() {
+    IFS=" "
+    set -- ${1}
+    createHttpResponse ${2#/}
+}
+#  awkの中で関数を実行したい →　どうしてもできない
+#  nc -l 3000 0<backpipe | awk '/HTTP/{system("bash -c " createHttpResponse substr($2, 2))}' 1>backpipe
+set -x
 while [[ true ]]; do
-#  awkの中で関数を実行したい
-  nc -l 3000 0<backpipe | awk '/HTTP/{system("bash -c 'createHttpResponse' substr($2, 2)")}' 1>backpipe
+    nc -l 3000 -w 1 0<backpipe | head -n1 | (read request; test "${request}") 1>backpipe
 done
+
