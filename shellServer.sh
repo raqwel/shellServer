cd47c6e75c91577b91449e70a35c7871d26973bf
raqwel <yuto.imamu@gmail.com>
2018-06-02 16:00:29 +0900

    ちょいりふぁくた

--- shellServer.sh
+++ shellServer.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 /usr/bin/mkfifo backpipe
+IFS=" "
 
 createHttpResponse() {
+    set -- ${1}
+    filename=${2#/}
+
     # HTTPレスポンスに必要なものを用意
     httpStatus="HTTP/1.1 200 OK"
     crlf="\r\n"
-    messageBody=$(cat ${1})
+    messageBody=$(cat ${filename})
     responseDate=$(LANG=en_US.UTF-8 date "+%a, %d %b %Y %T GMT")
     httpDate="Date: ${responseDate}"
     # HTTPレスポンス組み立て
@@ -24,6 +28,6 @@ test() {
 #  nc -l 3000 0<backpipe | awk '/HTTP/{system("bash -c " createHttpResponse substr($2, 2))}' 1>backpipe
 set -x
 while [[ true ]]; do
-    nc -l 3000 -w 1 0<backpipe | head -n1 | (read request; test "${request}") 1>backpipe
+    nc -l 3000 -w 1 0<backpipe | head -n1 | (read request; createHttpResponse "${request}") 1>backpipe
 done
 
