da883abfd96f112c9428ba341e161a98db9f0592
raqwel <yuto.imamu@gmail.com>
2018-06-24 14:10:00 +0900

    ディレクトリトラバーサルいけたっぽい

--- shellServer.sh
+++ shellServer.sh
@@ -3,12 +3,54 @@
 IFS=" "
 CRLF="\r\n"
 
+normalizePath() {
+    TARGET_FILE=$1
+
+    # 対象ファイルのディレクトリまで移動
+    # 一文字目に/があれば/を消したい　でも/index.htmlの場合は消したくない
+    if [[ ${TARGET_FILE:0:1} = / ]]; then
+        #echo "Hit"
+        TARGET_FILE=${TARGET_FILE#*/}
+        #echo ${TARGET_FILE}
+    fi
+
+#    currentDir=$(pwd)
+#
+#    echo ${currentDir}
+
+    # 対象ファイルのディレクトリ名を取得
+    cd `dirname "public/"${TARGET_FILE}`
+
+    # 対象ファイルのファイル名を取得
+    TARGET_FILE=${TARGET_FILE##*/}
+
+    # シンボリックリンクでないかを確認
+    while [ -L "${TARGET_FILE}" ]
+    do
+        TARGET_FILE=`readlink ${TARGET_FILE}`
+        cd `dirname ${TARGET_FILE}`
+        TARGET_FILE=`basename ${TARGET_FILE}`
+    done
+
+    PHYS_DIR=`pwd -P`
+    RESULT=${PHYS_DIR}/${TARGET_FILE}
+    echo ${RESULT}
+}
+
 setting404Response() {
     httpStatus="HTTP/1.1 404 Not Found"${CRLF}
     httpDate="Date: $(LANG=en_US.UTF-8 date -uR "+%a, %d %b %Y %T GMT")"${CRLF}
     contentType="Content-Type: text/html;charset=utf-8"${CRLF}
     contentLength="Content-Length: $(wc -c "./public/404.html" | awk '{print $1}')"${CRLF}
     messageBody=$(cat "./public/404.html")
+}
+
+setting403Response() {
+    httpStatus="HTTP/1.1 403 Forbidden"${CRLF}
+    httpDate="Date: $(LANG=en_US.UTF-8 date -uR "+%a, %d %b %Y %T GMT")"${CRLF}
+    contentType="Content-Type: text/html;charset=utf-8"${CRLF}
+    contentLength="Content-Length: $(wc -c "./public/403.html" | awk '{print $1}')"${CRLF}
+    messageBody=$(cat "./public/403.html")
 }
 
 setting200Response() {
@@ -31,14 +73,28 @@ showFileList() {
 
 createHttpResponse() {
     set -- ${1}
+
+    absolutePath=$(cd $(dirname $0)/public; pwd)
+
+    set -x
+    requestPath=$(normalizePath "${2}")
+
+    if [[ ! ${requestPath} =~ ${absolutePath} ]]; then
+        setting403Response
+        return;
+    fi
+    set +x
+
     fileName="public/${2#/}"
+
     # 拡張子がなければディレクトリとして読み込む
         #　以下にindex.htmlがあればそれを返す
         #　なければその中のファイルをリストとして返す
         #　ディレクトリ自体なければ404
     if [[ -z $(echo "${fileName}" | grep '\.') ]]; then
-        if [[ -e "${fileName}/index.html" ]]; then
-            setting200Response "${fileName}/index.html"
+        if [[ -e "${fileName}index.html" ]]; then
+            setting200Response "${fileName}index.html"
+            return;
         elif [[ -d "${fileName}" ]]; then
             showFileList "${fileName}"
             httpStatus="HTTP/1.1 200 OK"${CRLF}
@@ -47,15 +103,19 @@ createHttpResponse() {
             #contentLength="Content-Length: $(wc -c "${messageBody}" | awk '{print $1}')"${CRLF}
         else
             setting404Response
+            return;
         fi
     else
+
     # 拡張子があればそのファイルを読み込む
         # ファイルが有ればbodyレスポンスを返す
         # なければ404を返す
         if [[ -e "${fileName}" ]]; then
             setting200Response "${fileName}"
+            return;
         else
             setting404Response
+            return;
         fi
     fi
 }
@@ -72,7 +132,6 @@ returnHttpResponse() {
 
 #  awkの中で関数を実行したい →　どうしてもできない
 #  nc -l 3000 0<backpipe | awk '/HTTP/{system("bash -c " createHttpResponse substr($2, 2))}' 1>backpipe
-set -x
 while [[ true ]]; do
     # readがないと、HTTPリクエストがないのに処理が行き過ぎてしまうので、headの結果を待つ→リクエストをしっかり受け取ることを行う
     nc -l 3000 -w 1 0<backpipe | head -n1 | (read request; returnHttpResponse "${request}") 1>backpipe
