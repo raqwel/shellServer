29bb2de35f98ee9cd3481184267c82692519d982
raqwel <yuto.imamu@gmail.com>
2018-06-27 22:45:25 +0900

    ファイル削除・名前変更

--- HTTPRequest.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source HTTPResponse.sh
-
-#  awkの中で関数を実行したい →　どうしてもできない
-#  nc -l 3000 0<backpipe | awk '/HTTP/{system("bash -c " createHttpResponse substr($2, 2))}' 1>backpipe
-while [[ true ]]; do
-    # readがないと、HTTPリクエストがないのに処理が行き過ぎてしまうので、headの結果を待つ→リクエストをしっかり受け取ることを行う
-    nc -l 3000 -w 1 0<backpipe | head -n1 | (read request; writeHttpResponse "${request}") 1>backpipe
-done
--- shellServer.sh
+++ shellServer.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+source HTTPResponseGenerator.sh
 
 /usr/bin/mkfifo backpipe
 
-source ./HTTPRequest.sh
+#  awkの中で関数を実行したい →　どうしてもできない
+#  nc -l 3000 0<backpipe | awk '/HTTP/{system("bash -c " createHttpResponse substr($2, 2))}' 1>backpipe
+while [[ true ]]; do
+    # readがないと、HTTPリクエストがないのに処理が行き過ぎてしまうので、headの結果を待つ→リクエストをしっかり受け取ることを行う
+    nc -l 3000 -w 1 0<backpipe | head -n1 | (read request; writeHttpResponse "${request}") 1>backpipe
+done
